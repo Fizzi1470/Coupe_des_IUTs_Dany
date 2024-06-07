6c26625cea06c46bc0ff26cdad6ce7aa0c8087b7
sbellesoeurge1 <145007486+sbellesoeurge1@users.noreply.github.com>
2024-06-07 13:00:37 +0200

    ok-7-sirice

--- src/main.cpp
+++ src/main.cpp
@@ -119,10 +119,19 @@ void ligne_a_gauche(void){ // ligne détectée à gauche uniquement
 
 void croisement(void){ // croisement de lignes détecté
     compteur_croisements++;
+    if(compteur_croisements == 7)
+    {
+       u_turn();
+    }
+    if(compteur_croisements == 13)
+    {
+        perpandicular_turn(0);
+    }
 }
 
 void fin_de_ligne(void){ // sortie de piste détectée
-    u_turn();
+    pi.stop();
+    while(1);
 }
 
 void priorite_a_droite(void){
